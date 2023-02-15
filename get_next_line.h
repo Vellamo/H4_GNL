feea707307e1b5952bc90e9b0f560e5b87ab3a0b
lharvey <lharvey@student.hive.fi>
2023-02-15 11:50:08 +0200

    Style fixes

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lharvey <lharvey@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 10:00:18 by lharvey           #+#    #+#             */
-/*   Updated: 2023/02/15 11:28:58 by lharvey          ###   ########.fr       */
+/*   Updated: 2023/02/15 11:49:58 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 # include	<unistd.h>
 # include	<stdlib.h>
 
-void	ft_strdel(void **as);
-char	*ft_strchr(const char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, unsigned int len);
-char	*ft_strdup(const char *s1);
+void			ft_strdel(void **as);
+char			*ft_strchr(const char *s, int c);
+char			*ft_substr(char const *s, unsigned int start, unsigned int len);
+char			*ft_strdup(const char *s1);
 unsigned int	ft_strlen(const char *s);
-void	*ft_memcpy(void *dst, const void *src, unsigned int n);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*get_next_line(int fd);
+void			*ft_memcpy(void *dst, const void *src, unsigned int n);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*get_next_line(int fd);
 
 #endif
